b587bb0843de81cde47bcd114ad8b415ec2fd28e
vokbuda <112505771+vokbuda@users.noreply.github.com>
2023-07-21 21:46:46 +0200

    Added all necessary files for project evaluation

---
+++ js/hamburger.js
@@ -0,0 +1,17 @@
+function getMenu() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
+  //about---->  should go to the page:::What is pop exactly
+  //sale----->  choose your pop It
+  //contacts--->should go to the footer
+
+
+
+  // then u should implement the rest of all functions
